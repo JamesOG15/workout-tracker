b01545fc88bd3973a3650eb66b6e4895c154e095
JamesOG15 <james@themurphyfamily.co.uk>
2026-07-19 22:30:09 +0100

    Change theme, emojis, progress tab
    Now uploading the correct files

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'workout-tracker-shell-v2';
+const CACHE_NAME = 'workout-tracker-shell-v4';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
